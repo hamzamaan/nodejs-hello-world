a14331706e0cfe0b0e75c39013d882120e578224
Hamza Sajjad <41535104+hamzamaan@users.noreply.github.com>
2022-07-17 21:40:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 565;
+var port = 53;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
