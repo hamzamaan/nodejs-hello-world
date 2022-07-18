f0744a831ce8389127baf16d5c32a4989e68577c
Hamza Sajjad <41535104+hamzamaan@users.noreply.github.com>
2022-07-18 14:02:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-
 var http = require('http');
+
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+
+});
+
 var port = 80;
 server.listen(port);
 
