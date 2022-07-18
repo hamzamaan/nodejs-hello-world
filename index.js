f068430eedc7f249daf901236aa87e23fd577301
Hamza Sajjad <41535104+hamzamaan@users.noreply.github.com>
2022-07-18 13:57:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const http = require('http');
-
-const hostname = 'localhost';
-const port = 80;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 
 var http = require('http');
 var server = http.createServer(function(request, response) {
